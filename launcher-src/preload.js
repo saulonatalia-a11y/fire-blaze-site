c4d226c4be06595a1c29c3890649229b58cf3047
saulonatalia-a11y <saulo.natalia@gmail.com>
2026-09-11 14:06:24 -0300

    Add FIRE BLAZE Launcher installer pipeline

---
+++ launcher-src/preload.js
@@ -0,0 +1,12 @@
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('fireBlaze',{
+  state:()=>ipcRenderer.invoke('fb:state'),
+  login:(email,password)=>ipcRenderer.invoke('fb:login',email,password),
+  logout:()=>ipcRenderer.invoke('fb:logout'),
+  launch:()=>ipcRenderer.invoke('fb:launch'),
+  renew:method=>ipcRenderer.invoke('fb:renew',method),
+  refresh:()=>ipcRenderer.invoke('fb:refresh'),
+  openUpdate:url=>ipcRenderer.invoke('fb:open-update',url),
+  openCustomer:()=>ipcRenderer.invoke('fb:open-customer'),
+  onInstallProgress:fn=>ipcRenderer.on('fb:install-progress',(_e,data)=>fn(data))
+});
