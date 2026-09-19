745c505d5951a7b2b3dcbc38c1882e926f9fa981
saulonatalia-a11y <saulo.natalia@gmail.com>
2026-09-19 00:01:20 -0300

    Launcher V2: integrar preload.js funcional completo

--- launcher-v2-src/preload.js
+++ launcher-v2-src/preload.js
@@ -1,1 +1,16 @@
-const {contextBridge,ipcRenderer}=require('electron');contextBridge.exposeInMainWorld('launcherV2',{startup:()=>ipcRenderer.invoke('v2:startup'),login:(e,p)=>ipcRenderer.invoke('v2:login',e,p),logout:()=>ipcRenderer.invoke('v2:logout')});
+const { contextBridge, ipcRenderer } = require('electron');
+contextBridge.exposeInMainWorld('fireBlaze',{
+  state:()=>ipcRenderer.invoke('fb:state'),
+  login:(email,password)=>ipcRenderer.invoke('fb:login',email,password),
+  logout:()=>ipcRenderer.invoke('fb:logout'),
+  launch:()=>ipcRenderer.invoke('fb:launch'),
+  installMulti:()=>ipcRenderer.invoke('fb:install-multi'),
+  renew:method=>ipcRenderer.invoke('fb:renew',method),
+  refresh:()=>ipcRenderer.invoke('fb:refresh'),
+  openUpdate:url=>ipcRenderer.invoke('fb:open-update',url),
+  openCustomer:()=>ipcRenderer.invoke('fb:open-customer'),
+  updateLauncher:()=>ipcRenderer.invoke('fb:launcher-update'),
+  onLauncherUpdateProgress:cb=>ipcRenderer.on('fb:launcher-update-progress',(_e,p)=>cb(p)),
+  onInstallProgress:fn=>ipcRenderer.on('fb:install-progress',(_e,data)=>fn(data)),
+  onStateRefresh:cb=>ipcRenderer.on('fb:state-refresh',(_e,state)=>cb(state))
+});
